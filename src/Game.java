293dcc929d6d34c6d4ab5fd6bb261bb0ca8394a3
Bernardo Sampaio <119073813+bernsamp@users.noreply.github.com>
2023-01-09 02:24:48 +0000

    Added ranking system
    Added ranking system for final result based on the number of guesses. Maybe I'll update to a switch expression. 
    
    Also thinking about creating persistence in this game, creating player names and where you can check your previous games and ranks, and it will keep updating your game count and score. But that will come later.

--- src/Game.java
+++ src/Game.java
@@ -36,6 +36,21 @@ public class Game {
                 isCorrect = true;
             }
         }
+
+        if (counter == 1){
+            System.out.println("Hole in one! You're a true mind reader. 6 stars");
+        } else if (counter >= 2 && counter <= 5){
+            System.out.println("Amazing! 5 stars");
+        } else if (counter >= 6 && counter <= 10) {
+            System.out.println("Well done sir! 4 stars");
+        } else if (counter >= 11 && counter <= 15){
+            System.out.println("That was okay... 3 stars");
+        } else if (counter>=16 && counter <= 20){
+            System.out.println("That's just too many guesses... 2 stars");
+        } else {
+            System.out.println("Maybe you should try another game... 1 star");
+        }
+
         System.exit(0);
     }
 }
